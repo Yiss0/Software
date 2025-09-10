1dfa5c0fe07b99a4c91664ad50108735f424b7db
Jesús <jicg151@gmail.com>
2025-09-10 02:14:50 -0300

    login y registro

--- frontend/app/_layout.tsx
+++ frontend/app/_layout.tsx
@@ -1,14 +1,13 @@
-import { Stack } from "expo-router";
-import { StatusBar } from "expo-status-bar";
+import { Stack } from 'expo-router';
+import { StatusBar } from 'expo-status-bar';
+import { useFrameworkReady } from '@/hooks/useFrameworkReady';
 
-export default function RootLayout() {
+export default function Layout() {
   return (
-    <>
-      <Stack screenOptions={{ headerShown: false }}>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen name="+not-found" />
-      </Stack>
-      <StatusBar style="auto" />
-    </>
+    <Stack>
+      {/* Define la pantalla de login como la inicial */}
+      <Stack.Screen name="login" options={{ headerShown: false }} />
+      <Stack.Screen name="(tabs)/index" options={{ headerShown: false }} />
+    </Stack>
   );
 }
