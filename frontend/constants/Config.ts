8187aa5a767c00673e68fa9abad6ccf1f921951d
Jesús <jicg151@gmail.com>
2025-10-11 22:03:50 -0300

    Update Config.ts

--- frontend/constants/Config.ts
+++ frontend/constants/Config.ts
@@ -3,7 +3,7 @@
 // URL base de tu API.
 // El backend corre en el puerto 3001.
 // ¡MUY IMPORTANTE!: Cambia 'TU_IP_LOCAL' por la IP de tu computador en la red (ej: 192.168.1.100).
-const API_URL = 'http://192.168.100.3:3001';
+const API_URL = 'https://pastillapp-api.onrender.com';
 
 // Este ID ya no es necesario para la sincronización, pero es buena práctica tener el archivo de configuración.
 const MOCK_PATIENT_ID = 'id_de_prueba_si_lo_necesitas'; 
